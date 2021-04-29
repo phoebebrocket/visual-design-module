63b3c90839edf44ff80269032366c05b2c763a18
phoebebrocket <phoebebrocket@gmail.com>
2021-04-29 13:17:35 +0100

    code report fixes

--- js/weather.js
+++ js/weather.js
@@ -15,7 +15,7 @@ function jamWeather( cityID ) {
 
   function drawWeather( d ) {
 	let celcius = Math.round(parseFloat(d.main.temp)-273.15);
-	let fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+
 	
 	document.getElementById('description').innerHTML = d.weather[0].description;
 	document.getElementById('temp').innerHTML = celcius + '&deg;';
